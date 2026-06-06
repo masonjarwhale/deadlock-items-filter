5db104efb77a1906b624f542b43a3ab53aa8dadb
Localwizards <christiancampbell2002@gmail.com>
2026-06-06 02:31:23 -0700

    added lazy loading

--- graphic_grid.php
+++ graphic_grid.php
@@ -13,14 +13,14 @@ if (!empty($_POST['search']) ) {
     $data = $db->execute_query("SELECT filepath FROM (SELECT DISTINCT SUBSTRING_INDEX(graphic_name, '.', -2) AS graphic_name, filepath FROM $table) AS test WHERE filepath like ? GROUP BY graphic_name", [$search]);
 
     while($row = $data->fetch_assoc()){
-        echo "<a href='{$row['filepath']}'><image class='graphic' src='{$row['filepath']}'></image></a>";
+        echo "<a href='{$row['filepath']}'><image class='graphic' src='{$row['filepath']}' loading='lazy'></image></a>";
 
     }
 }
 else {
     $data = $db->query("SELECT filepath FROM (SELECT DISTINCT SUBSTRING_INDEX(graphic_name, '.', -2) AS graphic_name, filepath FROM $table) AS test GROUP BY graphic_name") or die($db->error);
     while($row = $data->fetch_assoc()){
-        echo "<a href='{$row['filepath']}'><image class='graphic' src='{$row['filepath']}'></image></a>";
+        echo "<a href='{$row['filepath']}'><image class='graphic' src='{$row['filepath']}' loading='lazy'></image></a>";
     }
 
 }
