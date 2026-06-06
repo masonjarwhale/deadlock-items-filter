709a4003bcda192a5ac2b21df80539eb940f3d78
Localwizards <christiancampbell2002@gmail.com>
2026-06-06 01:51:17 -0700

    fixed styling

--- about.php
+++ about.php
@@ -5,14 +5,14 @@
 </p>
 <p>
     All visuals created by Valve devs excluding a handful of SVGs I made by:<br>
-    â—‹ using preexisting SVGs (<a href="./icons/shred.svg" tabindex="0">shred</a> and <a href="./icons/armor_debuff_color.svg" tabindex="0">debuff resist</a> icon)<br>
-    â—‹ tracing bitmaps of preexisting PNGs (<a href="./icons/scaling.svg" tabindex="0">scaling</a>, <a href="./icons/fire_rate_jammer.svg" tabindex="0">enemy fire rate reduction</a>, and <a href="./icons/fire_rate_plus.svg" tabindex="0">fire rate</a> icon)<br>
+    ○ using preexisting SVGs (<a href="./icons/shred.svg" tabindex="0">shred</a> and <a href="./icons/armor_debuff_color.svg" tabindex="0">debuff resist</a> icon)<br>
+    ○ tracing bitmaps of preexisting PNGs (<a href="./icons/scaling.svg" tabindex="0">scaling</a>, <a href="./icons/fire_rate_jammer.svg" tabindex="0">enemy fire rate reduction</a>, and <a href="./icons/fire_rate_plus.svg" tabindex="0">fire rate</a> icon)<br>
 </p>
 <p>
     This site runs entirely on PHP, JQUERY AJAX, MySQL, CSS, and HTML.<br> 
     Check out the <a href="https://github.com/masonjarwhale/deadlock-items-filter" tabindex="0">source code</a>!
 </p>
 <p>
-    - Wizards (aka Deluxe) ðŸ«°
+    - Wizards (aka Deluxe) 🫰
     <a href="deadlockitems.cable199@simplelogin.com" tabindex="0">deadlockitems.cable199@simplelogin.com</a><br> 
 </p>
