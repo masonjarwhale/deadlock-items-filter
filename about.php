8fabffc5d13ef1c926cd5641eb24094097ff6345
Localwizards <christiancampbell2002@gmail.com>
2026-06-06 23:07:48 -0700

    brightness 2x on a:hover. updated description

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
 <p>
     All visuals created by Valve devs excluding a handful of SVGs I made by:<br>
     ○ using preexisting SVGs (<a href="./icons/shred.svg" tabindex="0">shred</a> and <a href="./icons/armor_debuff_color.svg" tabindex="0">debuff resist</a> icon)<br>
-    ○ tracing bitmaps of preexisting PNGs (<a href="./icons/scaling.svg" tabindex="0">scaling</a>, <a href="./icons/fire_rate_jammer.svg" tabindex="0">enemy fire rate reduction</a>, and <a href="./icons/fire_rate_plus.svg" tabindex="0">fire rate</a> icon)<br>
+    ○ tracing bitmaps of preexisting PNGs (<a href="./icons/scaling.svg" tabindex="0">scaling</a>, <a href="./icons/fire_rate_jammer.svg" tabindex="0">enemy fire rate reduction</a>, <a href="./icons/fire_rate_plus.svg" tabindex="0">fire rate</a>, <a href="./icons/icon_damage_mystic.svg" tabindex="0">imbue</a>, and <a href="./icons/active.svg" tabindex="0">active items</a> icon)<br>
 </p>
 <p>
     This site runs entirely on PHP, JQUERY AJAX, MySQL, CSS, and HTML.<br> 
