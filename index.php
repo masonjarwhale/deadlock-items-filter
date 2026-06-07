16ee0da33c3cabb97edd3b90773df1e9ecb70a4e
Localwizards <christiancampbell2002@gmail.com>
2026-06-06 22:27:59 -0700

    updated search bar placeholder

--- index.php
+++ index.php
@@ -86,7 +86,9 @@
 
                     $('.layout').css('grid-template-columns','14% 90%');
 
-                    $('.search-container').load("search_container.php");
+                    $('.search-container').load("search_container.php", {
+                        tab: current_table
+                    });
                     $('.search-title').html("<h2>IMAGES</h2><image class='info' title='PNG WEBP SVG ONLY&#013;&#013;ABRAMS ⟺ bull&#013;APOLLO ⟺ fencer&#013;BEBOP ⟺ bebop&#013;BILLY ⟺ punkgoat&#013;CALICO ⟺ nano&#013;CELESTE ⟺ unicorn&#013;DOORMAN ⟺ doorman&#013;DRIFTER ⟺ drifter&#013;DYNAMO ⟺sumo&#013;GRAVES ⟺ necro&#013;GREY TALON ⟺ archer&#013;HAZE ⟺ haze&#013;HOLLIDAY ⟺ Astro&#013;INFERNUS ⟺ inferno&#013;IVY ⟺ tengu&#013;KELVIN ⟺ kelvin&#013;LADY GEIST ⟺ spectre&#013;LASH ⟺ lash&#013;McGINNIS ⟺ engineer&#013;MINA ⟺ vampirebat&#013;MIRAGE ⟺ mirage&#013;MO & KRILL ⟺ digger&#013;PAIGE ⟺ bookworm&#013;PARADOX ⟺ chrono&#013;POCKET ⟺ synth&#013;REM ⟺ familiar&#013;SEVEN ⟺ gigawatt&#013;SHIV ⟺ shiv&#013;SILVER ⟺ werewolf&#013;SINCLAIR ⟺ magician&#013;VENATOR ⟺ priest&#013;VICTOR ⟺ frank&#013;VINDICTA ⟺ hornet&#013;VISCOUS ⟺ viscous&#013;VYPER ⟺ kali/viper&#013;WARDEN ⟺ warden&#013;WRAITH ⟺ wraith&#013;YAMATO ⟺ yamato' src='./icons/info.svg' tabindex='0'><image>");
                     $('.graphic-grid').load("graphic_grid.php", {
                         table: current_table
@@ -115,7 +117,9 @@
 
                     $('.layout').css('grid-template-columns','14% 90%');
 
-                    $('.search-container').load("search_container.php");
+                    $('.search-container').load("search_container.php", {
+                        tab: current_table
+                    });
                     $('.search-title').html("<h2>ICONS</h2><image class='info' title='PNG WEBP SVG ONLY&#013;&#013;ABRAMS ⟺ bull&#013;APOLLO ⟺ fencer&#013;BEBOP ⟺ bebop&#013;BILLY ⟺ punkgoat&#013;CALICO ⟺ nano&#013;CELESTE ⟺ unicorn&#013;DOORMAN ⟺ doorman&#013;DRIFTER ⟺ drifter&#013;DYNAMO ⟺sumo&#013;GRAVES ⟺ necro&#013;GREY TALON ⟺ archer&#013;HAZE ⟺ haze&#013;HOLLIDAY ⟺ Astro&#013;INFERNUS ⟺ inferno&#013;IVY ⟺ tengu&#013;KELVIN ⟺ kelvin&#013;LADY GEIST ⟺ spectre&#013;LASH ⟺ lash&#013;McGINNIS ⟺ engineer&#013;MINA ⟺ vampirebat&#013;MIRAGE ⟺ mirage&#013;MO & KRILL ⟺ digger&#013;PAIGE ⟺ bookworm&#013;PARADOX ⟺ chrono&#013;POCKET ⟺ synth&#013;REM ⟺ familiar&#013;SEVEN ⟺ gigawatt&#013;SHIV ⟺ shiv&#013;SILVER ⟺ werewolf&#013;SINCLAIR ⟺ magician&#013;VENATOR ⟺ priest&#013;VICTOR ⟺ frank&#013;VINDICTA ⟺ hornet&#013;VISCOUS ⟺ viscous&#013;VYPER ⟺ kali/viper&#013;WARDEN ⟺ warden&#013;WRAITH ⟺ wraith&#013;YAMATO ⟺ yamato' src='./icons/info.svg' tabindex='0'><image>");
                     $('.graphic-grid').load("graphic_grid.php", {
                         table: current_table
--- search_container.php
+++ search_container.php
@@ -1,2 +1,18 @@
-<input type="text" name="search" placeholder="ex: viscous%.webp"><br/>
-<button type="submit" name="submit">SEARCH</button>
+<?php
+
+include 'db.php';
+$tab = $_POST['tab'];
+
+if (!empty($_POST['tab']) ) {
+    if($tab === 'images') {
+        echo "<input type='text' name='search' placeholder='ex: gloat%.png'><br/>";
+        echo "<button type='submit' name='submit'>SEARCH</button>";
+    }
+    else {
+        echo "<input type='text' name='search' placeholder='ex: names%local%unfill'><br/>";
+        echo "<button type='submit' name='submit'>SEARCH</button>";
+    }
+}
+    
+
+?>
