a83c4974692500d152888451f87c3b830907bd58
Localwizards <christiancampbell2002@gmail.com>
2026-06-09 21:58:35 -0700

    removed search container <br>

--- php/search_container.php
+++ php/search_container.php
@@ -5,11 +5,11 @@ $tab = $_POST['tab'];
 
 if (!empty($_POST['tab']) ) {
     if($tab === 'images') {
-        echo "<input type='text' name='search' placeholder='ex: gloat%.png'><br/>";
+        echo "<input type='text' name='search' placeholder='ex: gloat%.png'>";
         echo "<button type='submit' name='submit'>SEARCH</button>";
     }
     else {
-        echo "<input type='text' name='search' placeholder='ex: names%local%unfill'><br/>";
+        echo "<input type='text' name='search' placeholder='ex: names%local%unfill'>";
         echo "<button type='submit' name='submit'>SEARCH</button>";
     }
 }
