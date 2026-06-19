3d20c37155dfa72f6b338a96bdb4801426c91f4e
Localwizards <christiancampbell2002@gmail.com>
2026-06-19 04:06:00 -0700

    improved item-dim on hover

---
+++ js/item_dim.js
@@ -0,0 +1,10 @@
+$(document).on('mouseenter focus', '.item, .item-dim', function(event) {
+    $('.item').attr('class','item-dim');
+    $(this).attr('class','item');
+    $('.item').css('transition','none');
+});
+
+$(document).on('mouseleave blur', '.item, .item-dim', function(event) {
+    $('.item-dim').attr('class','item');
+    $('.item').css('transition','filter 0.6s');
+});
