11dcb9762158ffaf4a3fbcc0e9d99f9a165b753b
Localwizards <christiancampbell2002@gmail.com>
2026-06-21 08:14:24 -0700

    reformatted code for edge case

--- js/item_dim.js
+++ js/item_dim.js
@@ -1,4 +1,5 @@
 $(document).on('mouseenter focus', '.item, .item-dim', function(event) {
+    $('.item').css('transition','filter 0.6s');
     $('.item').attr('class','item-dim');
     $(this).attr('class','item');
     $('.item').css('transition','none');
@@ -6,5 +7,4 @@ $(document).on('mouseenter focus', '.item, .item-dim', function(event) {
 
 $(document).on('mouseleave blur', '.item, .item-dim', function(event) {
     $('.item-dim').attr('class','item');
-    $('.item').css('transition','filter 0.6s');
 });
